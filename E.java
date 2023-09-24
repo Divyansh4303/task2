94d8e996c7991ecc1dbf2da0b38bb76444f68bd9
Divyansh4303 <divyansh.goyal2020@vitstudent.ac.in>
2023-09-24 21:41:14 +0530

    First

---
+++ E.java
@@ -0,0 +1,14 @@
+package Pack.Pack1;
+
+import java.util.*;
+import java.util.Scanner;
+
+public class E{
+    public int g;
+    public void right1(){
+        Scanner scan400 = new Scanner(System.in);
+        g=scan400.nextInt();
+        System.out.println(g>>5);
+        System.out.println("hiiii");
+    }
+}
